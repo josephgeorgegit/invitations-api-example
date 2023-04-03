37149a9f36a3fa192e9cc022f0c5486d8a082410
Joseph George <joseph@benda.app>
2023-04-03 12:20:08 +1000

    create-prompt embed url

--- create-prompt/app.js
+++ create-prompt/app.js
@@ -7,14 +7,14 @@ import { apiUrl, primaryKey, secretKey} from '../config.js'
  * to allow the recorder to emit event listeners to your page.
  */
 window.addEventListener('message', function(event){
-    if(event.origin !== 'http://dev.vpply.com:3000'){return}
+    if(event.origin !== 'https://interview.staging.vpply.com'){return}
     console.log(event.data) //these are the new prompts
 })
 let frame = document.createElement('iframe')
 frame.height = 800
 frame.width = 800
 frame.frameBorder = 0
-frame.src = `http://dev.vpply.com:3000/embed/create-question?api_key=${primaryKey}`
+frame.src = `https://interview.staging.vpply.com/embed/create-question?api_key=${primaryKey}`
 document.body.appendChild(frame)
 
 function promptsCreated(event){
